a7086290ae9b103a39da7bcd27d756b6a7376792
mason <hackcrisman@gmail.com>
2021-09-09 11:01:38 +0800

    Update scripts.go
    support external args for scripts plugin

--- scripts.go
+++ scripts.go
@@ -33,13 +33,16 @@ type command struct {
 // 设置脚本
 func setupScript(bot *Robot, in *Message) (msg []*Message) {
 	rule := in.Header.Get("rule")
+	extargs := in.Header.Get("args")[1]
 
 	scp := scripts[rule]
 
 	for _, sc := range scp.Command {
 		for _, c := range sc.Cmd {
 			args := strings.Split(c, " ")
-
+			if len(extargs) >0 {
+		        out, err := runCommand(sc.Dir,args[0],extargs...)	
+			}
 			out, err := runCommand(sc.Dir, args[0], args[1:]...)
 			if err != nil {
 				return NewMessages(err.Error())
